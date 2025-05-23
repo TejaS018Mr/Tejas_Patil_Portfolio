06201c7c2205363ae78c19c5eb2a3cfdbf215aef
TejaS018Mr <tejasteja012@gmail.com>
2025-05-23 15:42:23 +0530

    Fix base path configuration

--- tejas-patil-portfolio/vite.config.js
+++ tejas-patil-portfolio/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tejas_Patil_Portfolio/'
+  base: './',  // Change this line
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
